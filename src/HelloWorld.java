2dec96977d383d272cfc6080db92e8a9ea90b9d1
卓胜达 <719486159@qq.com>
2017-10-23 12:59:21 +0800

    idea commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,9 @@
  * 描述:
  * 这是一个测试类
  *
- * @outhor 71948
- * @create 2017-10-23 12:51
  */
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        System.out.println("HelloWolrd!");
+    }
 }
