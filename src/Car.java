f2ce86f286a511a8b1b78965d18f407705fff25b
卓胜达 <719486159@qq.com>
2017-10-23 13:03:54 +0800

    idea commit

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+/**
+ * 描述:
+ *
+ * @outhor 71948
+ * @create 2017-10-23 13:03
+ */
+public class Car {
+    public static void main(String[] args) {
+        System.out.println("这是一个车类");
+    }
+}
